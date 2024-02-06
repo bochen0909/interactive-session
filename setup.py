d5c9d22398a2564990e593d52510653c11b911be
Bo Chen <bochen0909@gmail.com>
2024-02-06 14:38:35 -0600

    add release action

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup, find_packages
-
-__version__ = "0.0.1"
+from version import __version__
 
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std=11/14/17, and then build_ext can be removed.
